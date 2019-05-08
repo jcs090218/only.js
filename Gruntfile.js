e4a0666aa2308541f761d3026fd96b7c2ec6ebad
Jen-Chieh <jcs090218@gmail.com>
2019-05-08 14:35:30 +0800

    Add grunt.js.

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function( grunt ) {
+  "use strict";
+
+  grunt.initConfig({
+    concat: {
+      js: {
+        src: ['src/*.js'],
+        dest: 'build/only.js'
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+};
