b32b27882411a5249642cb9b8334043805ea0277
jenchieh <jcs090218@gmail.com>
2019-05-10 09:21:43 +0800

    Add trans util

--- src/util.js
+++ src/util.js
@@ -31,3 +31,38 @@ only.Util.isNumber = function (sym) {
 only.Util.isString = function (sym) {
   return typeof sym == 'string';
 };
+
+/**
+ * Convert transform string to dictionary.
+ * @param { Array } mat : Transform array.
+ */
+only.Util.transToDic = function (mat) {
+  let values = mat[0];
+  let props = values.split(')').filter(Boolean);
+  let data = { };
+  props.forEach(function (prop) {
+    let pair = prop.split('(');
+    let key = pair[0].trim();
+    let val = pair[1].trim();
+    data[key] = val;
+  });
+  return data;
+};
+
+/**
+ * Convert dictionary to transform string.
+ * @param { Dictionary } dic : Dictionary data.
+ */
+only.Util.dicToTrans = function (dic) {
+  let result = '';
+  for (let p in dic) {
+    if (dic.hasOwnProperty(p)) {
+      let key = p;
+      let val = dic[p];
+      console.log(key);
+      console.log(val);
+      result += key + "(" + val + ") ";
+    }
+  }
+  return result;
+};
