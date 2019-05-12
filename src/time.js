eb019469a9f2ce1f37ffd81d70c2e56e4d380a0c
jenchieh <jcs090218@gmail.com>
2019-05-12 16:18:38 +0800

    Add time stamp.

--- src/time.js
+++ src/time.js
@@ -18,3 +18,26 @@ only.Time.FIXED_TIME = 0;
 only.Time.init = function () {
   only.Time.FIXED_TIME = 1 / only.Config.FPS;
 };
+
+only.Time.getFormattedDate = function () {
+  let monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+  let ampm = ["AM", "PM"];
+  let date = new Date();
+
+  let dd = date.getDate();
+  let mm = date.getMonth() + 1;
+  let mmIndex = mm;
+  let yy = date.getFullYear();
+
+  let hh = date.getHours();
+  let ampmIndex = Math.floor(hh / 12);
+  if (hh >= 12)
+    hh -= 12;
+  let min = date.getMinutes();
+  let ss = date.getSeconds();
+
+  return monthNames[mmIndex] + " " + dd + ", " + yy +
+    " " +
+    hh + ":" + min + ":" + ss + " " + ampm[ampmIndex];
+};
