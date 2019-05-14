af21ddf1e95ff10059b15f1b385f696f6718e388
Jen-Chieh <jcs090218@gmail.com>
2019-05-14 13:48:51 +0800

    Add transform examples.

---
+++ examples/movement/main.js
@@ -0,0 +1,42 @@
+/**
+ * $File: main.js $
+ * $Date: 2019-05-14 13:41:14 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2019 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+
+// Configure all the settings.
+only.Config.FPS = 30;
+only.Config.TITLE = "Movement Example";
+
+// Register all events.
+only.Event.registerInit(init);
+only.Event.registerUpdate(update);
+
+only.init();  // This should be last called.
+
+
+var disObj = null;
+
+function init () {
+  // Create display object ready for adding animation to it.
+  disObj = new only.Object('#disObj');
+
+  disObj.position = 'absolute';
+  disObj.width = 50;
+  disObj.height = 50;
+  disObj.top = 100;
+  disObj.left = 100;
+  disObj.backgroundColor = 'red';
+  disObj.opacity = 0.5;
+}
+
+function update () {
+  disObj.left += 100 * only.Time.FIXED_TIME;
+  // disObj.top += 100 * only.Time.FIXED_TIME;
+}
