958514f928376df6aca0a9c538f7e4eb77f39b68
Jen-Chieh <jcs090218@gmail.com>
2019-05-14 12:40:39 +0800

    Add resource module.

---
+++ src/resource.js
@@ -0,0 +1,16 @@
+/**
+ * $File: resource.js $
+ * $Date: 2019-05-14 12:24:49 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2019 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+if (typeof only === 'undefined') var only = { };
+
+only.Resource = { };
+only.Resource.PRELOADED_IMAGES = { };  // Preloaded Images
+only.Resource.PRELOADED_SOUNDS = { };  // Preloaded Sounds
