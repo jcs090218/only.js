3c990bdd288f3fb2e270d81f192a688a9e4dd4de
Jen-Chieh <jcs090218@gmail.com>
2019-05-15 14:13:02 +0800

    Add res checker.

--- src/resource.js
+++ src/resource.js
@@ -14,3 +14,19 @@ if (typeof only === 'undefined') var only = { };
 only.Resource = { };
 only.Resource.PRELOADED_IMAGES = { };  // Preloaded Images
 only.Resource.PRELOADED_SOUNDS = { };  // Preloaded Sounds
+
+only.Resource.LOADED_IMAGES_FLAGS = [];
+
+/* When all resource is loaded, call this. */
+only.Resource.loadedInit = function () {
+  if (only.Resource.allImagesLoaded()) {
+    only.Screen.INIT_RESIZE = true;
+    only.Screen.onResize();
+  }
+};
+
+/* Check if all images are loaded. */
+only.Resource.allImagesLoaded = function () {
+  return only.Resource.LOADED_IMAGES_FLAGS.length ==
+    only.Util.dictionaryLength(only.Resource.PRELOADED_IMAGES);
+};
