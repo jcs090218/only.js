811224ccc9ff640aa50cd128cdc1193a5aad908c
Jen-Chieh <jcs090218@gmail.com>
2019-06-03 16:10:07 +0800

    Add input.

---
+++ src/input.js
@@ -0,0 +1,19 @@
+/**
+ * $File: input.js $
+ * $Date: 2019-06-03 15:49:03 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2019 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+if (typeof only === 'undefined') var only = { };
+
+only.Input = { };
+
+
+only.Input.init = function () {
+
+};
